227606a2af46257c4bd00f25221b98f1cbe71856
Saffet Emin Reisoğlu <sereisoglu@gmail.com>
2022-07-28 23:52:18 +0300

    :construction: WIP project set up

---
+++ AppLogistAssignmentApp/AppLogistAssignmentApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  AppLogistAssignmentApp
+//
+//  Created by Saffet Emin Reisoğlu on 7/28/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
---
+++ AppLogistAssignmentApp/AppLogistAssignmentApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AppLogistAssignmentApp
+//
+//  Created by Saffet Emin Reisoğlu on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
